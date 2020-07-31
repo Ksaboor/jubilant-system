f5658977e17104167476d1be05f273b27412972e
Khalil Saboor <khalil.x.saboor@gsk.com>
2020-07-31 12:43:27 -0400

    firstcommit

---
+++ jubilant-system/Controllers/CMSController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace jubilant_system.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Produces("application/json")]
+    public class CMSController : ControllerBase
+    {
+
+        //GET: /<controller>/
+        [HttpGet]
+        public Object Get()
+        {
+            return new {
+                primaryColor = "#F36533",
+                secondaryColor = "#73645E",
+                tertiaryColor = "#FFFFFF",
+                quaternary = "#000000",
+                primaryFont = "GSK header button font",
+                secondaryFont = "GSK text, card font",
+                languageType = "us-english"
+            };
+        }
+    }
+}
---
+++ jubilant-system/Models/CMS.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace jubilant_system.Models
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+    public class Component
+    {
+        public string id { get; set; }
+        public string image { get; set; }
+        public string header { get; set; }
+        public string body { get; set; }
+        public string button { get; set; }
+        public string questions { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string mapImage { get; set; }
+        public string address { get; set; }
+        public string accept { get; set; }
+        public string denied { get; set; }
+    }
+
+    public class Page
+    {
+        public string order { get; set; }
+        public string title { get; set; }
+        public string active { get; set; }
+        public List<Component> components { get; set; }
+        public string languageType { get; set; }
+    }
+
+    public class CMS
+    {
+        public string primaryColor { get; set; }
+        public string secondaryColor { get; set; }
+        public string tertiaryColor { get; set; }
+        public string quaternary { get; set; }
+        public string primaryFont { get; set; }
+        public string secondaryFont { get; set; }
+        public string languageType { get; set; }
+        public List<Page> page { get; set; }
+    }
+
+
+}
